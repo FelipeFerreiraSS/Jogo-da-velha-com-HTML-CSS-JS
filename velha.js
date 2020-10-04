a52806ce63cf7e5b71ba664dce9e4e94a630407a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-10-04 20:02:42 -0300

    JS 2

--- velha.js
+++ velha.js
@@ -1,6 +1,25 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById(elementid: 'jogador-selecionado');
+
+mudarJogador( valor: 'X');
 
 function escolherQuadrado(id){
-    console.log(id);
-    
+
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+
+    if (jogador === 'X') {
+       jogador = 'o'; 
+    }else {
+        jogador = 'X';
+    }
+
+    mudarJogador(jogador);
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
